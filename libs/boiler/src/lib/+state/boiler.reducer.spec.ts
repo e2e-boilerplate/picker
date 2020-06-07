8582dfcf68d2e28839f7c5ddd9cb4d679820f91c
Girma Nigusse <xgirma@gmail.com>
2020-06-06 21:48:58 -0400

    updates

--- libs/boiler/src/lib/+state/boiler.reducer.spec.ts
+++ libs/boiler/src/lib/+state/boiler.reducer.spec.ts
@@ -1,35 +1,36 @@
-import { BoilerEntity } from './boiler.models';
 import * as BoilerActions from './boiler.actions';
 import { State, initialState, reducer } from './boiler.reducer';
 
-xdescribe('Boiler Reducer', () => {
-  const createBoilerEntity = (id: string, name = '') =>
-    // ({
-    //   id,
-    //   name: name || `name-${id}`,
-    // } as BoilerEntity);
-
-  beforeEach(() => {});
-
+describe('Boiler Reducer', () => {
   describe('valid Boiler actions', () => {
-    it('loadBoilerSuccess should return set the list of known Boiler', () => {
-      const boiler = [
-        createBoilerEntity('PRODUCT-AAA'),
-        createBoilerEntity('PRODUCT-zzz'),
-      ];
-      const action = BoilerActions.loadBoilerSuccess({ boiler });
+    it('should return boiled', () => {
+      const item = { platform: 'nodejs' };
+      const action = BoilerActions.boiled();
+      const actual: State = reducer(initialState, action);
+      const expected = {
+        framework: null,
+        platform: null
+      };
 
-      const result: State = reducer(initialState, action);
+      expect(actual.data).toEqual(expected);
+    });
 
-      expect(result.loaded).toBe(true);
-      expect(result.ids.length).toBe(2);
+    it('should update', () => {
+      const item = { platform: 'nodejs' };
+      const action = BoilerActions.update({ item });
+      const actual: State = reducer(initialState, action);
+      const expected = {
+        framework: null,
+        platform: 'nodejs'
+      };
+
+      expect(actual.data).toEqual(expected);
     });
   });
 
   describe('unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
-
       const result = reducer(initialState, action);
 
       expect(result).toBe(initialState);
--- libs/boiler/src/lib/+state/boiler.selectors.ts
+++ libs/boiler/src/lib/+state/boiler.selectors.ts
@@ -3,15 +3,8 @@ import {
   BOILER_FEATURE_KEY,
   State,
   BoilerPartialState,
-  boilerAdapter,
 } from './boiler.reducer';
 
 export const getBoilerState = createFeatureSelector<BoilerPartialState, State>(BOILER_FEATURE_KEY);
-
-const { selectAll, selectEntities } = boilerAdapter.getSelectors();
-
-export const getAllBoiler = createSelector(getBoilerState, (state: State) => selectAll(state));
-
-export const getBoilerEntities = createSelector(getBoilerState, (state: State) => selectEntities(state));
 
 export const getBoiled = createSelector(getBoilerState, (state: State) => state.data);
