38508e8b0582d1aff2b83ed517c926c5551af8a6
Girma Nigusse <xgirma@gmail.com>
2020-06-13 22:26:59 -0400

    updates

--- libs/core-data/src/lib/version/version.service.ts
+++ libs/core-data/src/lib/version/version.service.ts
@@ -3,13 +3,13 @@ import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
-import { UTILS_BASE_URL } from '@picker/constants';
+import { API_BASE_URL } from '@picker/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VersionService {
-  readonly BASE_URL = UTILS_BASE_URL;
+  readonly BASE_URL = API_BASE_URL;
   readonly model = 'dependencies';
 
   constructor(private httpClient: HttpClient) {}
@@ -17,7 +17,7 @@ export class VersionService {
   get(module): Observable<any> {
     const m = module === "webdriverio"? "@wdio/sync": module;
     return this.httpClient
-      .get<any>(`${this.BASE_URL}${this.model}.json`)
+      .get<any>(`${this.BASE_URL}${this.model}/index.json`)
       .pipe(map((response) => (response[m])));
   }
 }
