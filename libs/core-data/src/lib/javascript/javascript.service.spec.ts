700b030d74e8c41b71b09db5034346fddea18882
Girma Nigusse <xgirma@gmail.com>
2020-06-19 17:34:42 -0400

    updates

--- libs/core-data/src/lib/javascript/javascript.service.spec.ts
+++ libs/core-data/src/lib/javascript/javascript.service.spec.ts
@@ -2,13 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { JavascriptService } from './javascript.service';
+import { BoilerFacade, BoilerFacadeMock } from '@picker/boiler';
 
 xdescribe('JavascriptService', () => {
   let service: JavascriptService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [
+        { provide: BoilerFacade, useClass: BoilerFacadeMock}
+      ]
     });
     service = TestBed.inject(JavascriptService);
   });
