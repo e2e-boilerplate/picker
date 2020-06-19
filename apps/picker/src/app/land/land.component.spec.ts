1636bd452eaef20b1fdfe025bae27e706afc7327
Girma Nigusse <xgirma@gmail.com>
2020-06-19 17:35:26 -0400

    updates

--- apps/picker/src/app/land/land.component.spec.ts
+++ apps/picker/src/app/land/land.component.spec.ts
@@ -1,33 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-import { LandComponent } from './land.component';
-import { SharedModule } from '../shared/shared.module';
-import { BoilerFacade, BoilerFacadeMock } from '@picker/boiler';
-
-xdescribe('LandComponent', () => {
-  let component: LandComponent;
-  let fixture: ComponentFixture<LandComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, SharedModule, HttpClientTestingModule],
-      declarations: [ LandComponent ],
-      providers: [
-        { provide: BoilerFacade, useClass: BoilerFacadeMock}
-      ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LandComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { HttpClientTestingModule } from '@angular/common/http/testing';
+//
+// import { LandComponent } from './land.component';
+// import { SharedModule } from '../shared/shared.module';
+// import { BoilerFacade, BoilerFacadeMock } from '@picker/boiler';
+//
+// xdescribe('LandComponent', () => {
+//   let component: LandComponent;
+//   let fixture: ComponentFixture<LandComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       imports: [RouterTestingModule, SharedModule, HttpClientTestingModule],
+//       declarations: [ LandComponent ],
+//       providers: [
+//         { provide: BoilerFacade, useClass: BoilerFacadeMock}
+//       ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(LandComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
