4b1e0380561cb48435bd93fbea7a343db603d4e7
Girma Nigusse <xgirma@gmail.com>
2020-06-23 21:56:40 -0400

    updates

--- libs/services/src/lib/land/land.service.spec.ts
+++ libs/services/src/lib/land/land.service.spec.ts
@@ -2,7 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { LandService } from './land.service';
+import { landServiceMock } from '@picker/services';
 import { BoilerFacade, BoilerFacadeMock } from '@picker/boiler';
+import { LAND } from '@picker/constants';
 
 describe('LandService', () => {
   let service: LandService;
@@ -10,12 +12,25 @@ describe('LandService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [{ provide: BoilerFacade, useClass: BoilerFacadeMock}]
+      providers: [
+        { provide: BoilerFacade, useClass: BoilerFacadeMock},
+        { provide: LandService, useValue: landServiceMock }
+        ]
     });
     service = TestBed.inject(LandService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    expect(landServiceMock.path).toEqual('nodejs');
+  });
+
+  it('should return land data', (done) => {
+    const landServiceSpy = spyOn(landServiceMock, 'all').and.callThrough();
+    landServiceMock.all().subscribe( value => {
+      expect(value).toEqual(LAND);
+      expect(landServiceSpy).toHaveBeenCalledTimes(1);
+      done();
+    })
   });
 });
